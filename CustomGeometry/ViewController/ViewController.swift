3bab82af6d53042754c31bf4427f07b67f36bb18
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-08-08 15:32:01 +0900

    first commit

---
+++ CustomGeometry/ViewController/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  CustomGeometry
+//
+//  Created by 福山帆士 on 2020/08/08.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class ViewController: UIViewController {
+    
+    private let mySceneView: SCNView = {
+        let sceneView = SCNView()
+        return sceneView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        setupSceneView()
+        
+        settingView()
+        
+        let scene = SCNScene()
+        mySceneView.scene = scene
+        
+        addCameraNode(scene: scene)
+    }
+    
+    func setupSceneView() {
+        mySceneView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mySceneView)
+        let mySceneViewConstraits = [
+            mySceneView.topAnchor.constraint(equalTo: view.topAnchor),
+            mySceneView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            mySceneView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            mySceneView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ]
+        NSLayoutConstraint.activate(mySceneViewConstraits)
+    }
+    
+    func settingView() {
+        mySceneView.backgroundColor = #colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 1)
+        mySceneView.autoenablesDefaultLighting = true
+        mySceneView.allowsCameraControl = true
+        mySceneView.showsStatistics = true
+    }
+    
+    func addCameraNode(scene: SCNScene) {
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 11)
+        scene.rootNode.addChildNode(cameraNode)
+    }
+    
+    func createNode(scene: SCNNode) {
+        let harf = Float(2)
+        
+        let vertices = [
+            SCNVector3(-harf, harf, -harf), // 手前の4点
+            SCNVector3(-harf, -harf, -harf),
+            SCNVector3(harf, harf, -harf),
+            SCNVector3(harf, -harf, -harf),
+            
+            SCNVector3(-harf, harf, harf), // 奥の4点
+            SCNVector3(-harf, -harf, harf),
+            SCNVector3(harf, harf, harf),
+            SCNVector3(harf, -harf, harf)
+        ]
+        
+        let verticesSource = SCNGeometrySource(vertices: vertices)
+        
+    }
+    
+    
+
+
+}
+
---
+++ CustomGeometry/ViewModel/ViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ViewModel.swift
+//  CustomGeometry
+//
+//  Created by 福山帆士 on 2020/08/08.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import Foundation
